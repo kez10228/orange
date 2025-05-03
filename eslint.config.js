f08663f229d0a1c76bd2e82b599e044066219ba8
kez10228 <zhankevin8@gmail.com>
2025-05-03 19:26:46 +0100

    Update ESLint configuration and add ESLint plugin to webpack

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// filepath: c:\Users\zhank\Documents\Orange army\orange\.eslintrc.js
+module.exports = {
+  extends: "eslint:recommended",
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    // Add custom rules here if needed
+  },
+};
